51a7c73e4c9206712e1182ad8adec04a99a4e0d7
Sultan7rs <sultan7rs@gmail.com>
2023-04-26 23:04:53 -0700

    remove auth for now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+
 import { MantineProvider } from "@mantine/core";
 import ContextProvider from "../contexts/AppContext";
 import Layout from "../components/layout";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <MantineProvider
       withGlobalStyles
@@ -21,11 +17,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
       }}
     >
       <ContextProvider>
-        <SessionProvider session={session}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </SessionProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ContextProvider>
     </MantineProvider>
   );
