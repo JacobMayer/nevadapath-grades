465741b4896b17fda16555e8178af275f3334673
Sultan7rs <sultan7rs@gmail.com>
2023-05-02 14:37:06 -0700

    ui/ux optimizations

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,22 +4,24 @@ import Link from "next/link";
 
 function PageNotFound() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-6 py-2">
-      <div className="flex flex-wrap-reverse">
-        <div className="flex-auto">
-          <h1 className="text-4xl font-bold">404</h1>
-          <h2 className="text-2xl font-bold">Page Not Found</h2>
-          <p className="mb-8 mt-4 text-xl font-light leading-relaxed">
-            Oops! The page you are looking for does not exist. It might have
-            been moved or deleted.
-          </p>
-          <Link href="/" passHref>
-            <Button variant="light">Go Home</Button>
-          </Link>
-        </div>
+    <div className="container mx-auto px-12 sm:px-36">
+      <div className="flex min-h-screen flex-col justify-center py-6">
+        <div className="flex flex-wrap-reverse">
+          <div className="flex-auto">
+            <h1 className="text-4xl font-bold">404</h1>
+            <h2 className="text-2xl font-bold">Page Not Found</h2>
+            <p className="mb-8 mt-4 text-xl font-light leading-relaxed">
+              Oops! The page you are looking for does not exist.
+              <br /> It might have been moved or deleted.
+            </p>
+            <Link href="/" passHref>
+              <Button variant="light">Go Home</Button>
+            </Link>
+          </div>
 
-        <div className="w-64 flex-auto">
-          <NotFoundSvgComponent />
+          <div className="w-3/4 shrink lg:max-w-md">
+            <NotFoundSvgComponent />
+          </div>
         </div>
       </div>
     </div>
